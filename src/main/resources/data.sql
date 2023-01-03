9cd22938acad9b486e08bebc4191fd76f758d227
pbborisov18 <pbborisov18@codingburgas.bg>
2023-01-03 21:25:50 +0200

    yes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee (name, email) VALUES ('ivan1', 'ivan@Email1');
+INSERT INTO employee (name, email) VALUES ('ivan2', 'ivan@Email2');
+INSERT INTO employee (name, email) VALUES ('ivan3', 'ivan@Email3');
+INSERT INTO employee (name, email) VALUES ('ivan4', 'ivan@Email4');
+INSERT INTO employee (name, email) VALUES ('ivan5', 'ivan@Email5');
